eaf84867e2b3ce06f0c02496b1b5287da69767b6
LVar97 <letkovaa@mail.ru>
2021-07-31 11:05:41 +0300

    added final styles & fonts

---
+++ src/components/Final.js
@@ -0,0 +1,10 @@
+function Final(props){
+	return(
+		<div className="promo">
+			<h1 className="promo__final-title">ЗАЯВКА ПРИНЯТА</h1>
+			<p className="promo__final-text">Держите телефон под рукой.  Скоро с Вами свяжется наш менеджер.</p>
+		</div>
+	);
+}
+
+export default Final;
